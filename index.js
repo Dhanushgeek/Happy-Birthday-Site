7fbdebf615fd70d551527496f04aa3bd56f35c9a
Dhanushgeek <dhanushgeek@gmail.com>
2020-12-20 05:02:55 +0530

    updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector(".btn").addEventListener("click", function(){
     var bdaySong=new Audio("sounds/birthday.mp3");
-    bdaySong.play();
-    alert("Touch the card");
+       bdaySong.play();
+       alert("Touch the card");
 })
